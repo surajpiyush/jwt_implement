bf625b0fd516c3915221ccd09157a91dbc561129
piyush kumar <piyushkumar9648@gmail.com>
2023-02-14 14:28:37 +0530

    second

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,30 +1,55 @@
+// import { Injectable } from '@nestjs/common';
+
+// export type User = any;
+
+// @Injectable()
+// export class UsersService {
+//   private readonly users: User[];
+
+//   constructor() {
+//     this.users = [
+//       {
+//         userId: 1,
+//         username: 'piyush',
+//         password: '123',
+//       },
+//       {
+//         userId: 2,
+//         username: 'laukush',
+//         password: '123',
+//       },
+//       {
+//         userId: 3,
+//         username: 'manik',
+//         password: '123',
+//       },
+//     ];
+//   }
+
+//   async findOne(username: string): Promise<User | undefined> {
+//     return this.users.find(user => user.username === username);
+//   }
+// }
+
 import { Injectable } from '@nestjs/common';
 
+// This should be a real class/interface representing a user entity
 export type User = any;
 
 @Injectable()
 export class UsersService {
-  private readonly users: User[];
-
-  constructor() {
-    this.users = [
-      {
-        userId: 1,
-        username: 'piyush',
-        password: '123',
-      },
-      {
-        userId: 2,
-        username: 'laukush',
-        password: '123',
-      },
-      {
-        userId: 3,
-        username: 'manik',
-        password: '123',
-      },
-    ];
-  }
+  private readonly users = [
+    {
+      userId: 1,
+      username: 'piyush',
+      password: '123',
+    },
+    {
+      userId: 2,
+      username: 'laukush',
+      password: '123',
+    },
+  ];
 
   async findOne(username: string): Promise<User | undefined> {
     return this.users.find(user => user.username === username);
